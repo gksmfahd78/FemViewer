5cb606214a41f9549c1ec9fadc9fb04171602b9b
gksmfahd78 <dlehdgus0413@gmail.com>
2021-01-25 16:14:32 +0900

    직선, 곡선 추가

--- app/src/main/java/com/pj/femviewer/MainActivity.java
+++ app/src/main/java/com/pj/femviewer/MainActivity.java
@@ -3,6 +3,9 @@ package com.pj.femviewer;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
 import android.os.Bundle;
 
 import com.opencsv.CSVReader;
@@ -15,6 +18,8 @@ public class MainActivity extends AppCompatActivity implements FileData {
 
     private Variable variable = new Variable();
     private Canvas canvas = new Canvas();
+    private Paint paint = new Paint();
+    private Path path = new Path();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -64,11 +69,31 @@ public class MainActivity extends AppCompatActivity implements FileData {
     }
 
     public void straightLine(int startX, int startY, int endX, int endY) {
-        //straight_line
+        path.moveTo(startX, startY);
+        path.lineTo(endX, endY);
+        canvas.drawPath(path, paint);
     }
 
-    public void curvedLine(int startX, int startY, int endX, int end) {
-        //curved_line
+    public void curvedLine(int startX, int startY, int endX, int endY) {
+        int mx1 = (endX-startX)/2,
+                my1 = endY,
+                mx2 = (endX-startX)/2,
+                my2 = startY;
 
+        drawOption();
+        path.moveTo(startX, startY);
+        path.cubicTo(mx1, my1, mx2, my2, endX, endY);
+        canvas.drawPath(path, paint);
+    }
+
+    public void drawOption() {
+        paint.setColor(Color.BLACK);
+        paint.setStrokeWidth(3);
+        paint.setStyle(Paint.Style.STROKE);
+    }
+
+    public void drawClear() {
+        path.reset();
+        canvas.drawPath(path, paint);
     }
 }
